7e2b7ab54057d1224abe1d8a072923b7f73dfa9e
johndgiese <johndgiese@gmail.com>
2013-06-01 23:44:01 -0400

    Finish coding and testing basic functionality.

--- bootstrap-datedropdown.js
+++ bootstrap-datedropdown.js
@@ -1,20 +1,83 @@
 (function($){
-    $.fn.dateDropdown = function(options){
 
+    // UTILITY FUNCTIONS
+    var date2string = function(date) {
+        return (date.getMonth() + 1) + "/" + date.getDate() + "/" + date.getFullYear();
+    };
+
+    var string2date = function(str) {
+        return new Date(str);
+    };
+
+    var parseDateOption = function(opt) {
+        if (typeof(opt) == 'string') {
+            return string2date(opt);
+        } else {
+            return opt;
+        }
+    };
+
+    var getTextForCurrentValue = function(date, choice) {
+        var text;
+        switch (choice.type) {
+            case 'constant':
+                text = date2string(choice.value);
+                break;
+            case 'function':
+                text = choice.date2text(date);
+                break;
+            case 'yearsAfter':
+                text = String(date.getFullYear() - choice.origin.getFullYear());
+                break;
+            case 'pickDate':
+                text = date2string(date);
+                break;
+            default:
+                throw "Unreconized choice type."
+        }
+        return text;
+    };
+
+    // note, this isn't perfectly accurate
+    var yearsBetweenDates = function(date1, date2) {
+        return date1.getFullYear() - date2.getFullYear();
+    };
+
+    // DROPDOWN PLUGIN
+    var initDropdown = function($dropdown, options) {
+
+        // PARSE OPTIONS
+        
         // defaults
-        var settings = $.extend({
+        var settings = $.extend(true, {
             dropdownPosition: "left",
             defaultChoice: 0,
             inputId: "",
+            initialValue: new Date('today'),
             class: "date-dropdown",
         }, options || {});
 
-        if (settings.dropdownPosition == "left") {
-            var alignmentClass = "input-prepend";
-        } else {
-            var alignmentClass = "input-append";
+        settings.currentChoice = settings.choices[settings.defaultChoice];
+        settings.currentValue = new Date(settings.initialValue.getTime());
+
+        // convert date-strings to date-objects
+        for (var i = 0; i < settings.choices.length; i++) {
+            var choice = settings.choices[i];
+
+            switch (choice.type) {
+                case 'yearsAfter':
+                    choice.origin = parseDateOption(choice.origin);
+                    break;
+                case 'constant':
+                    choice.value = parseDateOption(choice.value);
+                    break;
+            }
         }
 
+        // save a copy of choices onto the object
+        $dropdown.data('settings', settings);
+
+        // GENERATE HTML
         var $choices = $("<ul>").addClass("dropdown-menu");
         for (var i = 0; i < settings.choices.length; i++) {
             var choice = settings.choices[i];
@@ -28,7 +91,7 @@
         var $btn = $("<button>")
         .addClass("btn dropdown-toggle")
         .attr("data-toggle", "dropdown")
-        .append('<span class="current-option">')
+        .append('<span class="current-option-label">')
         .append('<span class="caret">');
 
         var $btnGroup = $("<div>")
@@ -39,15 +102,21 @@
         .prop("id", settings.inputId)
         .prop("type", "text");
 
-        var $dropdown = $(this)
-        .addClass(alignmentClass)
-        .addClass(settings.class);
+        if (settings.dropdownPosition == "left") {
+            var alignmentClass = "input-prepend";
+        } else {
+            var alignmentClass = "input-append";
+        }
+
+        $dropdown.addClass(alignmentClass)
+        .addClass(settings.class)
         if (settings.dropdownPosition == "left") {
             $dropdown.append($btnGroup, $input);
         } else {
             $dropdown.append($input, $btnGroup);
         }
 
+        // SETUP VAL FUNCTIONS
         // override the val() function, preserving previous functionality in
         // case of conflicts with other plugins unless the div has the
         // date-dropdown class
@@ -68,18 +137,83 @@
                         return "";
                     }
                 }
-                return $(elem).find('input').val();
+                return date2string(settings.currentValue);
             },
             set: function(elem, val) {
                 if (!$(elem).hasClass(settings.class)) {
                     if (origHookSet) {
                         origHookSet(elem, val);
-                        return $(this);
+                        return $dropdown;
                     }
                 }
-                return $(elem).find('input').val(val);
+                settings.currentValue = parseDateOption(val);
+                updateCurrentValue();
+                return $dropdown;
             }
         };
 
+        // CHANGE LISTENERS
+        var updateCurrentValue = function() {
+            var date;
+            var choice = settings.currentChoice;
+            var text = $input.val();
+            switch (choice.type) {
+                case 'constant':
+                    date = choice.value;
+                    break;
+                case 'function':
+                    date = choice.text2date(text);
+                    break;
+                case 'yearsAfter':
+                    var yearsAfter = Number(text);
+                    date = new Date(choice.origin.getTime());
+                    date.setYear(date.getFullYear() + yearsAfter);
+                    break;
+                case 'pickDate':
+                    date = new Date(text);
+                    break;
+                default:
+                    throw "Unreconized choice type."
+            }
+            return date;
+        };
+
+        $input.change(function(){
+            var date = updateCurrentValue();
+            settings.currentValue = date;
+        });
+
+        $dropdown.find('li').click(function(){
+            var choiceNumber = $(this).prevAll('li').size();
+            $dropdown.dateDropdown('choice', choiceNumber);
+        });
+    };
+
+    var changeDropdownState = function($dropdown, choiceNumber) {
+        var settings = $dropdown.data().settings;
+        var $input = $dropdown.find('input');
+        var choice = settings.choices[choiceNumber];
+        settings.currentChoice = choice;
+
+        if (choice.type == 'constant') {
+            settings.currentValue = choice.value;
+        }
+
+        var date = settings.currentValue;
+        var text = getTextForCurrentValue(date, choice);
+        $input.val(text);
+        $dropdown.find('.current-option-label').text(choice.label + " ");
+    };
+
+    $.fn.dateDropdown = function(){
+        $dropdown = $(this);
+
+        if (arguments.length == 1) {
+            initDropdown($dropdown, arguments[0]);
+        }
+        else if (arguments.length == 2 && arguments[0] == 'choice') {
+            changeDropdownState($dropdown, arguments[1]);
+        }
+        return $dropdown;
     };
 })(jQuery);
